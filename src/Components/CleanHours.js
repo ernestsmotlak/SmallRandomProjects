adbdd0271b59d6881b635c76accb1cbf9b10a578
ernestsmotlak <ernestsmotlak@gmail.com>
2024-03-14 16:06:10 +0100

    Created a compnent to clean string and compute hours worked and money earned.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

---
+++ src/Components/CleanHours.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import './CleanHours.css';
+
+const CleanHours = () => {
+    const [showMoneyDiv, setShowMoneyDiv] = useState(false);
+    const [money1, setMoney1] = useState(0);
+    const [money2, setMoney2] = useState(0);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        var inputText = document.getElementById('inputText').value;
+        var cleanedText = inputText.replaceAll('ure', '').replaceAll('uri', '').replaceAll('ur', '');
+        var arr = cleanedText.split('  ');
+
+        let sum = parseInt(0);
+        for (let i = 0; i < arr.length; i++) {
+            sum = sum + parseInt(arr[i]);
+        }
+
+        setMoney1(sum * 5.8);
+        setMoney2(sum * 6.2);
+        setShowMoneyDiv(true);
+
+    };
+    return (
+
+        <div className='cleanHours'>CleanHours
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="inputText" />
+                <br />
+                <br />
+                <input type="submit" value="Submit" className='btnForm'/>
+            </form>
+
+            <br /><br />
+
+            {showMoneyDiv && (
+                <div>
+                    <div>Money: {money1}</div>
+                    <div>Possible money: {money2}</div>
+                </div>
+            )}
+
+        </div>
+    )
+}
+
+export default CleanHours
