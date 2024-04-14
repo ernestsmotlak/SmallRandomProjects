410d3aaebe5b7dd8fea03f1654e388c6fc67d905
ernestsmotlak <ernestsmotlak@gmail.com>
2024-04-14 21:11:04 +0200

    added usenavigate instead of href
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- server.js
+++ server.js
@@ -7,51 +7,53 @@ const PORT = process.env.PORT || 3000;
 http.createServer((req, res) => {
     let filePath = '';
 
-    // Serve static files
+    // Serve index.html for all routes
     if (req.url === '/') {
-        // Serve index.html for the root URL
         filePath = path.join(__dirname, 'build', 'index.html');
     } else {
-        // Serve other static files
+        // Serve other static files (like JavaScript, CSS, images) from the build directory
         filePath = path.join(__dirname, 'build', req.url);
     }
 
-    // Read the file and serve it
-    fs.readFile(filePath, (err, data) => {
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
         if (err) {
             res.writeHead(404);
             res.end('Not Found');
             return;
         }
 
-        // Set content type based on file extension
-        const extname = path.extname(filePath);
-        let contentType = 'text/html';
-        switch (extname) {
-            case '.js':
-                contentType = 'text/javascript';
-                break;
-            case '.css':
-                contentType = 'text/css';
-                break;
-            case '.png':
-                contentType = 'image/png';
-                break;
-            case '.jpg':
-                contentType = 'image/jpeg';
-                break;
-            case '.gif':
-                contentType = 'image/gif';
-                break;
-            case '.svg':
-                contentType = 'image/svg+xml';
-                break;
-        }
+        // Read the file and serve it
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Server Error');
+                return;
+            }
 
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(data);
+            // Set appropriate content type based on file extension
+            const extname = path.extname(filePath);
+            let contentType = 'text/html';
+            switch (extname) {
+                case '.js':
+                    contentType = 'text/javascript';
+                    break;
+                case '.css':
+                    contentType = 'text/css';
+                    break;
+                case '.json':
+                    contentType = 'application/json';
+                    break;
+                case '.png':
+                    contentType = 'image/png';
+                    break;
+                // Add cases for other file types as needed
+            }
+
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        });
     });
 }).listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const MainRouter = () => {
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate('/TimetableClean');
+    };
+
     return (
         <div className='d-flex justify-content-center'>
             <form className="card" style={{ width: '30rem' }}>
@@ -9,8 +16,14 @@ const MainRouter = () => {
                     <div>
                         <label className="form-label">Choose Your Preffered Functionality:</label>
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item"><p>
-                                <a className="link-offset-3 link-primary link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover" href="/TimetableClean">Timetable Tool</a></p>
+                            <li className="list-group-item">
+                                <p>
+                                    <a className="link-offset-3 link-primary link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover"
+                                        onClick={handleNavigate}
+                                        style={{ cursor: 'pointer' }}>
+                                        Timetable Tool
+                                    </a>
+                                </p>
                             </li>
                             {/* <li className="list-group-item">
                                 <p><a className="link-offset-3 link-primary link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover" href="#">Offset 3 link</a></p>
