f0b21599bd7b1af8945617e8befc7de1979b37a1
ernestsmotlak <ernestsmotlak@gmail.com>
2024-04-14 15:38:33 +0200

    Finished the urnik component
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/Projects/Urnik.js
+++ src/Projects/Urnik.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Urnik = () => {
+    const [inputtedString, setInputtedString] = useState('');
+    const [finalSum, setFinalSum] = useState(0);
+
+    const cleanString = () => {
+        if (inputtedString !== null) {
+            const numbersArray = inputtedString.match(/\d+/g);
+            console.log("Prvic: ", numbersArray);
+
+            let sum = 0;
+            if (numbersArray) {
+                for (let i = 0; i < numbersArray.length; i++) {
+                    sum += parseInt(numbersArray[i], 10);
+                }
+                setFinalSum(sum);
+            }
+            else {
+                console.log('No numbers found!');
+                setFinalSum(0); // Reset the sum if no numbers found
+            }
+        }
+        else {
+            console.log('No String!');
+        }
+    };
+
     return (
-        <div className='urnik'>
-            <div className="card" style={{ width: '18rem' }}>
+        <div className='urnik d-flex justify-content-center'>
+            <form className="card" style={{ width: '30rem' }}>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="card-link">Card link</a>
-                    <a href="#" className="card-link">Another link</a>
+                    <h5 className="card-title">Get Hours/Money</h5>
+                    <div>
+                        <label className="form-label">Enter the text here:</label> <br />
+                        <input type='text' value={inputtedString} onChange={(e) => setInputtedString(e.target.value)}>
+                        </input>
+                        <br />
+                        <button type='button' onClick={cleanString} className='mt-3 btn btn-primary'>Submit</button>
+                    </div>
                 </div>
-            </div>
+                {finalSum > 0 ? finalSum : null}
+            </form>
+
         </div>
     );
 };
